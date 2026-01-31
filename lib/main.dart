115e2108e65c86a2369dcbe9045119d360a69502
Nokibul82 <nokibul82@gmail.com>
2026-01-31 15:15:31 +0600

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Peanut Trading',
         theme: ThemeData(
           primarySwatch: Colors.blue,
